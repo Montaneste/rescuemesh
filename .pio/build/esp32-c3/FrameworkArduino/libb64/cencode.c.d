e19f347993c773680c0ff02b4afa035f3be1db02
Joao Monteiro <joao.monteiro.portugal@gmail.com>
2026-09-24 21:12:53 +0100

    RescueMesh baseline: ESP32 leak detection and valve control

--- .pio/build/esp32-c3/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-c3/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/Hackathon/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/Hackathon/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
